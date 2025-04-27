9be84f6e19481abf0ab89962ba985513106a5e7a
Thomas Q <thomasqsa@gmail.com>
2025-04-27 13:06:11 -0300

    Remove non doc related files

--- set-perms.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Probably not necessary for every docker setup, but definitely necessary if you're running SE Linux
-mkdir -p grafana-data
-sudo chown -R 472:472 grafana-data provisioning dashboards
-sudo chmod -R 755 grafana-data provisioning dashboards
-sudo chmod 644 grafana.ini
